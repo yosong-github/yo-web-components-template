1a9c9d45b9bbe4eec2103977b7646e55fcfcf4d0
yosong <2404559603@qq.com>
2025-03-17 16:45:32 +0800

    feat: 新增打包为单文件

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vitePluginCompileLessInTemplate from './plugin/vite-plugin-compile-less-in-template'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // 打包成单页面
 export default defineConfig({
@@ -15,7 +16,11 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  plugins: [vitePluginCompileLessInTemplate()],
+  plugins: [
+    // 打包单文件
+    viteSingleFile(),
+    vitePluginCompileLessInTemplate()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
